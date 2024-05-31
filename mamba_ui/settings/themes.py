dc13dbb3312825368d95ae19723745b72738dfb2
ChrisUnice <chrisunice@gmail.com>
2024-05-31 13:40:02 -0700

    connect polar plot behavior to change with theme and fixed issue with dropdown being white some times.

--- mamba_ui/settings/themes.py
+++ mamba_ui/settings/themes.py
@@ -6,6 +6,8 @@ from dash_extensions.enrich import Input, Output, State, MATCH
 from mamba_ui import app
 from mamba_ui import config
 
+# Component
+
 AVAILABLE_THEMES = sorted(config['themes']['light'] + config['themes']['dark'])
 
 label = html.Label(
@@ -20,7 +22,9 @@ dropdown = dcc.Dropdown(
     placeholder=config['themes']['default'],
     options=[
         {'label': theme, 'value': getattr(dbc.themes, theme)} for theme in AVAILABLE_THEMES
-    ]
+    ],
+    clearable=False,
+    style={'color': 'gray'}
 )
 
 ThemeDropdownRow = dbc.Row(
@@ -30,6 +34,9 @@ ThemeDropdownRow = dbc.Row(
     ],
     align='center'
 )
+
+
+# Callbacks
 
 
 @app.callback(
@@ -57,36 +64,69 @@ def update_linear_plot_theme(theme_value, figure, theme_options):
         theme_map = {option['value']: option['label'] for option in theme_options}
         theme_name = theme_map[theme_value]
         if theme_name in config['themes']['light']:
-            layout_updates = {
-                'font': {'color': 'black'},
-                'yaxis': {
-                    'gridcolor': 'gray',
-                    'zerolinecolor': 'black',
-                    'zerolinewidth': 2,
-                },
-                'xaxis': {
-                    'gridcolor': 'gray',
-                    'zerolinecolor': 'black',
-                    'zerolinewidth': 2,
-                }
+            layout_updates = {'font': {'color': 'black'}}
+            xaxis_updates = {
+                'gridcolor': 'gray',
+                'zerolinecolor': 'black'
             }
-        elif theme_name in config['themes']['dark']:
-            layout_updates = {
-                'font': {'color': 'white'},
-                'yaxis': {
-                    'gridcolor': 'lightgray',
-                    'zerolinecolor': 'white',
-                    'zerolinewidth': 2
-
-                },
-                'xaxis': {
-                    'gridcolor': 'lightgray',
-                    'zerolinecolor': 'white',
-                    'zerolinewidth': 2,
-
+            yaxis_updates = {
+                'gridcolor': 'gray',
+                'zerolinecolor': 'black'
                 }
+        elif theme_name in config['themes']['dark']:
+            layout_updates = {'font': {'color': 'white'}}
+            xaxis_updates = {
+                'gridcolor': 'lightgray',
+                'zerolinecolor': 'white'
+            }
+            yaxis_updates = {
+                'gridcolor': 'lightgray',
+                'zerolinecolor': 'white'
             }
         else:
-            layout_updates = {}
+            raise PreventUpdate
         figure['layout'].update(layout_updates)
+        figure['layout']['xaxis'].update(xaxis_updates)
+        figure['layout']['yaxis'].update(yaxis_updates)
+        return figure
+
+
+@app.callback(
+    Output({'type': 'polar-plot', 'index': MATCH}, 'figure'),
+    Input('theme-dropdown', 'value'),
+    State({'type': 'polar-plot', 'index': MATCH}, 'figure'),
+    State('theme-dropdown', 'options')
+)
+def update_polar_plot_theme(theme_value, figure, theme_options):
+    """ Updates the styling of the polar plot based on the overall application theme """
+    if theme_value is None:
+        raise PreventUpdate
+    else:
+        theme_map = {option['value']: option['label'] for option in theme_options}
+        theme_name = theme_map[theme_value]
+        if theme_name in config['themes']['light']:
+            layout_updates = {'font': {'color': 'black'}}
+            angular_updates = {
+                'gridcolor': 'gray',
+                'linecolor': 'gray'
+            }
+            radial_updates = {
+                'gridcolor': 'gray',
+                'linecolor': 'black'
+            }
+        elif theme_name in config['themes']['dark']:
+            layout_updates = {'font': {'color': 'white'}}
+            angular_updates = {
+                'gridcolor': 'lightgray',
+                'linecolor': 'lightgray'
+            }
+            radial_updates = {
+                'gridcolor': 'lightgray',
+                'linecolor': 'white'
+            }
+        else:
+            raise PreventUpdate
+        figure['layout'].update(layout_updates)
+        figure['layout']['polar']['angularaxis'].update(angular_updates)
+        figure['layout']['polar']['radialaxis'].update(radial_updates)
         return figure
