dd70f0eeca03cb502b7dcf5b8ba0b9ea739430fd
ChrisUnice <chrisunice@gmail.com>
2024-06-04 16:03:33 -0700

    refactoring to a class component

--- mamba_ui/settings/window.py
+++ mamba_ui/settings/window.py
@@ -1,26 +1,54 @@
 from dash import dcc, html
 import dash_bootstrap_components as dbc
 
-from mamba_ui.components import HorizontalLine
+from mamba_ui.components.base import BaseComponent
 from mamba_ui.settings.themes import ThemeDropdownRow
 from mamba_ui.settings.gridshape import GridShapeRow
+from mamba_ui.components.lines import HorizontalLineComponent
 
 
-SettingsWindow = dbc.Modal(
-    [
-        dcc.Store('settings-store', storage_type='memory'),
-        dbc.ModalHeader(html.H2('Settings')),
-        dbc.ModalBody(
-            [
+class SettingsWindow(BaseComponent):
+
+    def __init__(self):
+        super().__init__()
+
+    @property
+    def _header(self) -> dbc.ModalHeader:
+        return dbc.ModalHeader(
+            children=[
+                html.H2('Settings')
+            ]
+        )
+
+    @property
+    def _body(self) -> dbc.ModalBody:
+        body_style = {
+            'height': '50vh'
+        }
+
+        return dbc.ModalBody(
+            children=[
                 ThemeDropdownRow,
-                HorizontalLine('sm'),
+                HorizontalLineComponent('sm').component,
                 GridShapeRow,
-                HorizontalLine('sm')
+                HorizontalLineComponent('sm').component
             ],
-            style={'height': '50vh'}
-        ),
-        dbc.ModalFooter()
-    ],
-    id='settings-window',
-    size='lg'
-)
+            style=body_style
+        )
+
+    @property
+    def _footer(self) -> dbc.ModalFooter:
+        return dbc.ModalFooter()
+
+    @property
+    def component(self) -> dbc.Modal:
+        return dbc.Modal(
+            children=[
+                dcc.Store('settings-store', storage_type='memory'),
+                self._header,
+                self._body,
+                self._footer
+            ],
+            id='settings-window',
+            size='lg'
+        )
