d68e7c8baa3256321440b7547c4b06e156554026
ChrisUnice <chrisunice@gmail.com>
2024-06-05 12:25:10 -0700

    explict imports

--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,12 @@
-from backend_callbacks import *
+import mamba_ui as mui
+
+from backend.display_widgets import display_widget
+from backend.create_test_session import create_test_client
+from backend.populate_uploads import populate_uploads
 
 
 if __name__ == '__main__':
 
+    # Serve dash app
     mui.app.layout = mui.serve_layout()
     mui.app.run(debug=True, port=8050)
