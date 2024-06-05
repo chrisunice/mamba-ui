95228d3cc06e50503fd66b5024521d60fd2b6237
ChrisUnice <chrisunice@gmail.com>
2024-06-05 14:27:42 -0700

    simplified base plot widget to get the plot menu component

--- mamba_ui/widgets/plots/base.py
+++ mamba_ui/widgets/plots/base.py
@@ -1,9 +1,7 @@
-import dash_uploader as du
 from abc import abstractmethod
-import dash_bootstrap_components as dbc
 
 from mamba_ui.widgets.base import BaseWidget
-from mamba_ui.components.submit_button_group import SubmitButtonGroupComponent
+from mamba_ui.widgets.plots.menu import PlotMenuComponent
 
 
 class BasePlotWidget(BaseWidget):
@@ -20,31 +18,6 @@ class BasePlotWidget(BaseWidget):
         pass
 
     @property
-    def _menu(self) -> list:
+    def menu(self):
         """ Common menu items used between plots """
-
-        accordion_style = {
-            'width': '100%'
-        }
-
-        return [
-            dbc.Accordion(
-                className='text-dark',
-                children=[
-                    dbc.AccordionItem(
-                        children=[
-                            du.Upload(
-                                id={'type': 'widget-menu-data-upload', 'index': self.index},
-                                text='Upload Data',
-                            )
-                        ],
-                        title='Data'
-                    ),
-                    dbc.AccordionItem([], title='Filters'),
-                    dbc.AccordionItem([], title='Design'),
-                    dbc.AccordionItem([], title='Export')
-                ],
-                style=accordion_style
-            ),
-            SubmitButtonGroupComponent(name='widget', index=self.index).component
-        ]
+        return PlotMenuComponent(self.index).component
