2eb01ddb77330e9f51a0c41342e310411f3c6eab
ChrisUnice <chrisunice@gmail.com>
2024-06-06 11:17:22 -0700

    added name and uid to base component

--- mamba_ui/components/base.py
+++ mamba_ui/components/base.py
@@ -2,10 +2,20 @@ from abc import ABC, abstractmethod
 
 
 class BaseComponent(ABC):
+
+    name = 'Base Component'
+
     def __init__(self):
         super().__init__()
+
+    @property
+    def uid(self):
+        """ Unique ID for dash components """
+        return '-'.join(self.name.lower().split())
 
     @property
     @abstractmethod
     def component(self):
         pass
+
+
