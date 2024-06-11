fc9e9538db5ec0c24e6d1f750e29255d2548cc21
ChrisUnice <chrisunice@gmail.com>
2024-06-11 15:51:46 -0700

    adding callback to populate freq and pol with some data

--- tests/test.py
+++ tests/test.py
@@ -3,6 +3,7 @@ import mamba_ui as mui
 from backend.display_widgets import display_widget
 from backend.create_test_session import create_test_client
 from backend.populate_uploads import populate_uploads
+from backend.populate_menu_filters import populate_menu_filters
 
 
 if __name__ == '__main__':
