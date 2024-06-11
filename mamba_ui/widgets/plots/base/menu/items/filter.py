c9f730258aca681b4f4f31da6076d33aa183f925
ChrisUnice <chrisunice@gmail.com>
2024-06-11 15:54:18 -0700

    adding containers around the menu items so callbacks can be connected later

--- mamba_ui/widgets/plots/base/menu/items/filter.py
+++ mamba_ui/widgets/plots/base/menu/items/filter.py
@@ -2,48 +2,70 @@ from dash import dcc, html
 import dash_bootstrap_components as dbc
 
 from mamba_ui.components.base import BaseComponent
+from mamba_ui.components.dropdown_checklist import DropdownChecklist
 
 
 class PlotMenuFilterItemComponent(BaseComponent):
-    def __init__(self, index: str = ''):
+    def __init__(
+            self,
+            categorical_filters: dict = None,
+            numerical_filters: dict = None,
+            index: str = ''
+    ):
+        """
+        :param filters: a dictionary describing the filtering
+            keys - filter names that match columns in the data (i.e. frequency, polarization)
+            values - data nature (i.e. discrete or continuous)
+        :param index:
+        """
         super().__init__()
+        if categorical_filters is None:
+            categorical_filters = {}
+        self.categorical_filters = categorical_filters
+
+        if numerical_filters is None:
+            numerical_filters = {}
+        self.numerical_filters = numerical_filters
+
         self.index = index
 
-    @property
-    def _checklist(self) -> dcc.Checklist:
-        checklist_style = {
-            'maxHeight': '300px',
-            'overflowY': 'auto'
-        }
+    def _build_categorical_filter(self, name: str, options: list):
+        """ Builds a dropdown checklist component """
 
-        input_style = {
-            'marginRight': '10px'
-        }
-
-        label_style = {
+        style = {
             'display': 'flex',
-            'alignItems': 'center'
+            'flexDirection': 'row',
+            'alignItems': 'center',
+            'width': '100%',
+            'minHeight': '50px',
+            'height': '100%'
         }
 
-        return dcc.Checklist(
-            id={'type': 'plot-menu-filter-checklist', 'index': self.index},
-            options=[],
-            style=checklist_style,
-            inputStyle=input_style,
-            labelStyle=label_style
+        return html.Div(
+            children=[
+                html.Div(
+                    html.Label(name.capitalize(), style={'fontSize': 'larger'}),
+                    style={'width': '50%'}
+                ),
+                html.Div(
+                    DropdownChecklist(name, options=options, index=self.index).component,
+                    style={'width': '50%'}
+                )
+            ],
+            style=style
         )
+
+    def _build_numerical_filter(self, name: str):
+        """ Builds a range slider component """
+        return
 
     @property
     def component(self) -> dbc.AccordionItem:
+
+        filter_items = [self._build_categorical_filter(k, v) for k, v in self.categorical_filters.items()] + \
+                       [self._build_numerical_filter(k, v) for k, v in self.numerical_filters.items()]
+
         return dbc.AccordionItem(
-            children=[
-                dbc.Accordion(
-                    children=[
-                        dbc.AccordionItem([], title='Frequency'),
-                        dbc.AccordionItem([], title='Polarization')
-                    ]
-                ),
-                # self._checklist
-            ],
+            children=filter_items,
             title=html.H4('Filter')
         )
