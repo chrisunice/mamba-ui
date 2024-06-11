0e050420b4da79c1821f0f44b3a58653a1d09740
ChrisUnice <chrisunice@gmail.com>
2024-06-11 15:53:31 -0700

    got DropdownChecklist component working with callbacks too

--- mamba_ui/components/dropdown_checklist.py
+++ mamba_ui/components/dropdown_checklist.py
@@ -1,79 +1,103 @@
-from dash import html, dcc
+from dash import callback_context
 import dash_bootstrap_components as dbc
+from dash.exceptions import PreventUpdate
+from dash_extensions.enrich import Input, Output, MATCH, ALL
+
+from mamba_ui import app
+from mamba_ui.components.base import BaseComponent
 
 
-def dropdown_checklist(
-        id_name: str,
-        options: list | dict = None,
-        style: dict = None,
-        menu_item_kwargs: dict = None,
-        checklist_kwargs: dict = None
-):
-    id_name = id_name.lower()
+class DropdownChecklist(BaseComponent):
+    def __init__(self, name: str, options: list = None, index: str = ''):
+        super().__init__()
+        self.name = name
+        if options is None:
+            options = []
+        self.options = options
 
-    # Handle keyword arguments
-    if style is None:
-        style = {}
+        self.id = {
+            'parent-component': f'{self.name}-dropdown-checklist',
+            'index': index
+        }
 
-    if menu_item_kwargs is None:
-        menu_item_kwargs = {'toggle': False}
+    @property
+    def _checklist(self):
+        """ dbc.Checklist sub component """
+        checklist_id = self.id.copy()
+        checklist_id.update({'child-component': 'checklist'})
+        return dbc.Checklist(
+            id=checklist_id,
+            options=self.options,
+            className='dropdown-checklist-checklist'
+        )
 
-    if checklist_kwargs is None:
-        checklist_kwargs = {}
+    @property
+    def _dropdown_item(self):
+        """ dbc.DropdownMenuItem sub component """
 
-    # Styling component
-    menu_style = {
-        'display': 'flex',
-        'justify-content': 'space-between',
-        'align-items': 'center',
-        'width': '100%',
-        'color': 'black',
-        'background': 'white',
-    }
+        style = {
+            'overflowY': 'auto',
+            'maxHeight': '250px',
+            'width': '100%',
+        }
 
-    menu_item_style = {'background-color': 'transparent', 'overflow-y': 'auto', 'max-height': '250px'}
+        item_id = self.id.copy()
+        item_id.update({'child-component': 'menu-item'})
 
-    checklist_style = {'display': 'flex', 'flexDirection': 'column'}
+        return dbc.DropdownMenuItem(
+            className='dropdown-checklist-menu-item',
+            id=item_id,
+            children=[self._checklist],
+            toggle=False,
+            style=style
+        )
 
-    checklist_input_style = {'margin-right': '10px'}
+    @property
+    def component(self):
 
-    checklist_label_style = {'color': 'black', 'font-size': 'large'}
+        toggle_style = {
+            'display': 'flex',
+            'justify-content': 'space-between',
+            'align-items': 'center',
+            'width': '100%',
+        }
 
-    # Handle if no items are passed to the dropdown component
-    if options is None:
-        options = ['No results found']
-        checklist_input_style['display'] = 'none'
+        menu_id = self.id.copy()
+        menu_id.update({'child-component': 'menu'})
 
-    # Build component
-    component = html.Div(
-        id=id_name,
-        children=[
-            dbc.DropdownMenu(
-                id=f'{id_name}-menu',
-                label='Select...',
-                children=[
-                    dbc.DropdownMenuItem(
-                        id=f'{id_name}-menu-item',
-                        children=[
-                            dcc.Checklist(
-                                id=f'{id_name}-checklist',
-                                options=options,
-                                style=checklist_style,
-                                inputStyle=checklist_input_style,
-                                labelStyle=checklist_label_style,
-                                persistence=True,
-                                persistence_type='session',
-                                **checklist_kwargs
-                            )
-                        ],
-                        style=menu_item_style,
-                        **menu_item_kwargs
-                    )
-                ],
-                toggle_style=menu_style
-            )
-        ],
-        style=style
-    )
+        return dbc.DropdownMenu(
+            id=menu_id,
+            children=[
+                self._dropdown_item
+            ],
+            label='Select...',
+            color='secondary',
+            toggle_style=toggle_style
+        )
 
-    return component
+
+@app.callback(
+    Output({'parent-component': ALL, 'child-component': 'menu', 'index': MATCH}, 'label'),
+    Input({'parent-component': ALL, 'child-component': 'checklist', 'index': MATCH}, 'value')
+)
+def update_dropdown_label(updates: list) -> list:
+    if callback_context.triggered_id is None:
+        raise PreventUpdate
+
+    default_value = 'Select...'
+    new_updates = []
+    for update in updates:
+        try:
+            num_selections = len(update)
+        except TypeError:
+            # update is None
+            new_updates.append(default_value)
+        else:
+            if num_selections == 0:
+                # zero items selected
+                new_updates.append(default_value)
+            else:
+                # multiple items selected
+                new_updates.append(f'{num_selections} selected')
+
+    return new_updates
