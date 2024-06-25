ac2432f3d5412578ab406a8250904f89a9166dca
ChrisUnice <chrisunice@gmail.com>
2024-06-25 15:06:26 -0700

    updated to new syntax, settings window opens and closes properly

--- tests/backend/settings/toggle_settings_window.py
+++ tests/backend/settings/toggle_settings_window.py
@@ -5,12 +5,18 @@ import mamba_ui as mui
 
 
 @mui.app.callback(
-    Output('settings-window', 'is_open'),
+    Output(
+        component_id={'name': 'settings-window', 'index': ''},
+        component_property='is_open'
+    ),
     Input(
         component_id={'name': 'navbar', 'type': 'settings-icon', 'index': ''},
         component_property='n_clicks'
     ),
-    Trigger('dash-layout', 'children')
+    Trigger(
+        component_id='dash-layout',
+        component_property='children'
+    )
 )
 def toggle_settings_window(click):
     if click is not None:
