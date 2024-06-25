3c1a440963d50eb15f14a018da9bd688df6245e6
ChrisUnice <chrisunice@gmail.com>
2024-06-25 07:58:19 -0700

    updated path to imports

--- tests/test.py
+++ tests/test.py
@@ -1,11 +1,12 @@
 import mamba_ui as mui
 
-from backend.create_test_session import create_test_client
-from backend.display_widgets import display_widget
-from backend.populate_menu_filters import populate_menu_filters
-from backend.populate_menu_display import populate_menu_display
-from backend.plot_linear_data import plot_linear_data
-from backend.close_sidebar_on_submit import close_sidebar_on_submit
+from backend.general.create_test_session import create_test_client
+from backend.general.display_widgets import display_widget
+from backend.general.close_sidebar_on_submit import close_sidebar_on_submit
+
+from backend.plots.populate_menu_filters import populate_menu_filters
+from backend.plots.populate_menu_display import populate_menu_display
+from backend.plots.plot_linear_data import plot_linear_data
 
 if __name__ == '__main__':
 
