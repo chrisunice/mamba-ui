43b426fe159cc8d30b90466ed625564c2479548c
ChrisUnice <chrisunice@gmail.com>
2024-06-26 09:37:20 -0700

    updated doc string

--- mamba_ui/widgets/base.py
+++ mamba_ui/widgets/base.py
@@ -8,7 +8,12 @@ class BaseWidget(BaseComponent):
     name = 'Base Widget'
 
     def __init__(self, index: str, name: str = None):
-        """ Wrapper for BaseComponent that adds menu as a mandatory property just like component """
+        """
+        Wrapper for BaseComponent that adds menu as a mandatory property just like component
+
+        :param index: a unique index that should specify the location of the component
+        :param name: the name of the component; if no name is given then the class attribute name will be used
+        """
         super().__init__(name=name, index=index)
 
     @property
